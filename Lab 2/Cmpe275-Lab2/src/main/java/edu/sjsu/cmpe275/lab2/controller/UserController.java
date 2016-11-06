24d824c08e6ad00f50e88ac5a79a671ee689179d
Onkar <ganjewaronkar@gmail.com>
2016-11-05 20:32:44 -0700

    Remove sample views
    Removed all the sample .jsp files.

--- Lab 2/Cmpe275-Lab2/src/main/java/edu/sjsu/cmpe275/lab2/controller/UserController.java
+++ Lab 2/Cmpe275-Lab2/src/main/java/edu/sjsu/cmpe275/lab2/controller/UserController.java
@@ -9,21 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * @author Onkar Ganjewar
- * Cmpe275-Lab2
+ * @author Onkar Ganjewar Cmpe275-Lab2
  */
 
 @Controller
 public class UserController {
-	
-	@RequestMapping("/jsptest")
-    public String test(ModelAndView modelAndView) {
-        
-        return "jsp-spring-boot";
-  }
-  @RequestMapping("/demo/{user_name}")
-	public String helloWorld(final Map<String, Object> model, @PathVariable("user_name") String userName) throws Exception {
-		String name = "Hello, "+userName;
+
+//	@RequestMapping("/jsptest")
+//	public String test(ModelAndView modelAndView) {
+//		return "index";
+//	}
+
+	@RequestMapping("/demo/{user_name}")
+	public String helloWorld(final Map<String, Object> model, @PathVariable("user_name") String userName)
+			throws Exception {
+		String name = "Hello, " + userName;
 		model.put("msg", name);
 		return "helloWorld";
 	}
