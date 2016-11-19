13a6122e08db3bf5ba299a56427f2b740db0afac
Onkar Ganjewar <ganjewaronkar@gmail.com>
2016-11-18 21:51:45 -0800

    Test renderHome method of UserController.java
    Successfully tested renderHome of UserController. Also, modified pom.xml
    to add JUnit and Mockito dependencies

--- Lab 2/Cmpe275-Lab2/src/test/java/edu/sjsu/cmpe275/lab2/controller/UserControllerTest.java
+++ Lab 2/Cmpe275-Lab2/src/test/java/edu/sjsu/cmpe275/lab2/controller/UserControllerTest.java
@@ -3,73 +3,82 @@ package edu.sjsu.cmpe275.lab2.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
+//import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
-import edu.sjsu.cmpe275.lab2.model.User;
 /**
  * 
  * @author Shreya Prabhu Cmpe275-Lab2
  *
  */
+// @ContextConfiguration(loader = AnnotationConfigContextLoader.class,
+// classes = { AppConfig.class, ServletConfig.class })
+// "classpath:**/WEB-INF/spring-context.xml"
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes= UserController.class)
-
+@ContextConfiguration(locations = { "classpath:**/META-INF/persistence.xml",
+		"file:src/main/webapp/WEB-INF/spring-context.xml", })
 @WebAppConfiguration
-
 public class UserControllerTest {
+
 	private MockMvc objMockMvc;
-	
-	private User user;
-	
-	private MediaType contentType=new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(),
-			Charset.forName("utf8"));
-	
-	@Autowired
-	private WebApplicationContext webApplicationContext;
-	
+
+//	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+//			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+	// @Autowired
+	// private ApplicationContext context;
+
+	@InjectMocks
+	private UserController userController;
+
+	// @Autowired
+	// private WebApplicationContext webApplicationContext;
+
 	@Before
-	public void setup()throws Exception{
-		
-		this.objMockMvc=webAppContextSetup(webApplicationContext).build();
-		
+	public void setup() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		// this.objMockMvc =
+		// MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+		this.objMockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+
 	}
-	
+
 	@Test
-	public void test_renderHome() throws Exception{
-		objMockMvc.perform(get("/"))  
-				.andExpect(status().isOk())
-				.andExpect(MockMvcResultMatchers.forwardedUrl("WEB-INF/views/home.jsp"));
-		
+	public void RenderHomeTest() throws Exception {
+		this.objMockMvc.perform(MockMvcRequestBuilders.get("/user")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("home")).andDo(print());
 	}
-	
+
 	@Test
-	public void test_getUser() throws Exception{
-		objMockMvc.perform(get("/user/20"))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(contentType));
-		
+	public void test_getUser() throws Exception {
+		objMockMvc.perform(MockMvcRequestBuilders.get("/user/26")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("showUser")).andDo(print());
 	}
-	
+
+	// @Test
+	// public void test_renderHome() throws Exception{
+	// objMockMvc.perform(get("/user"))
+	// .andExpect(status().isOk())
+	// .andExpect(MockMvcResultMatchers.forwardedUrl("home.jsp"));
+	//
+	// }
+
+	// public void setApplicationContext(ApplicationContext applicationContext)
+	// throws BeansException {
+	// // TODO Auto-generated method stub
+	// context = applicationContext;
+	// }
+
 }
