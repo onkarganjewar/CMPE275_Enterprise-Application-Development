b5761fdaf15555b5adbcd2eef207755c558fe343
Shreya Prabhu <Shreya@Shreyas-MacBook-Pro.local>
2016-11-19 12:30:10 -0800

    Updated phonecontrollertest file
    Added RenderPhoneHomeTest function.

--- Lab 2/Cmpe275-Lab2/src/test/java/edu/sjsu/cmpe275/lab2/controller/PhoneControllerTest.java
+++ Lab 2/Cmpe275-Lab2/src/test/java/edu/sjsu/cmpe275/lab2/controller/PhoneControllerTest.java
@@ -1,5 +1,46 @@
 package edu.sjsu.cmpe275.lab2.controller;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+/**
+ * 
+ * @author Shreya Prabhu Cmpe275-Lab2
+ *
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:**/META-INF/persistence.xml",
+		"file:src/main/webapp/WEB-INF/spring-context.xml", })
+@WebAppConfiguration
+
 public class PhoneControllerTest {
+	private MockMvc objMockMvc;
+	@InjectMocks
+	private PhoneController phoneController;
+	
+	@Before
+	public void setup() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		this.objMockMvc = MockMvcBuilders.standaloneSetup(phoneController).build();
+
+	}
+	
+	@Test
+	public void RenderPhoneHomeTest() throws Exception {
+		this.objMockMvc.perform(MockMvcRequestBuilders.get("/phone")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("phoneIndex")).andDo(print());
+	}
 
 }
